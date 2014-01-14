03c40ed7e0c79d87baa47f7e87d6b3f5eff78da5
wrathematics <wrathematics@gmail.com>
2014-01-14 10:30:46 -0500

    01.14.14 static lib finally fucking builds right

---
+++ src/install.libs.R
@@ -0,0 +1,33 @@
+### Modified from pbdSLAP/src/install.libs.R
+
+files <- c("SEXPtools.so", "SEXPtools.so.dSYM", "SEXPtools.dylib", "SEXPtools.dll", "SEXPtools.rds")
+
+files <- files[file.exists(files)]
+
+if(length(files) > 0){
+  libsarch <- if (nzchar(R_ARCH)) paste("libs", R_ARCH, sep='') else "libs"
+  dest <- file.path(R_PACKAGE_DIR, libsarch)
+  dir.create(dest, recursive = TRUE, showWarnings = FALSE)
+  file.copy(files, dest, overwrite = TRUE, recursive = TRUE)
+}
+
+### For lib
+file <- "libsexptools.a"
+
+if(file.exists(file)){  ### External libraries do not build "libslap.a"
+  libarch <- if (nzchar(R_ARCH)) paste("lib", R_ARCH, sep='') else "lib"
+  dest <- file.path(R_PACKAGE_DIR, libarch)
+  dir.create(dest, recursive = TRUE, showWarnings = FALSE)
+  file.copy(file, dest, overwrite = TRUE)
+}
+
+### For etc
+file <- "Makeconf"
+
+if(file.exists(file)){
+  etcarch <- if (nzchar(R_ARCH)) paste("etc", R_ARCH, sep='') else "etc"
+  dest <- file.path(R_PACKAGE_DIR, etcarch)
+  dir.create(dest, recursive = TRUE, showWarnings = FALSE)
+  file.copy(file, dest, overwrite = TRUE)
+}
+
