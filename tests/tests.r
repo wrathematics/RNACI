5bc19bff226e53ba81925675a0a79899b563e80b
Drew Schmidt <schmidt@math.utk.edu>
2014-01-15 21:26:28 -0500

    01.15.14 fixed tests

--- tests/tests.r
+++ tests/tests.r
@@ -1,11 +1,23 @@
 library(Rtools, quietly=TRUE)
 
-# Lists
-test_list(TRUE)
-test_list(FALSE)
 
-# Printing
+### Lists
+a <- list(abc=1:2, defg=-10.10214)
+b <- test_list(TRUE)
+all.equal(a, b)
+
+names(a) <- NULL
+b <- test_list(FALSE)
+all.equal(a, b)
+
+
+### Dataframes
+a <- data.frame(X1=1:2, X2=c(-10.10214, 1.23456))
+b <- test_df()
+all.equal(a, b)
+
+
+### Printing
 test_print()
 
-# Dataframes
-test_df()
+
