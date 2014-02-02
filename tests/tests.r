4d605fafc56a644c2c01396b2b6edb693f232e07
Drew Schmidt <schmidt@math.utk.edu>
2014-02-02 14:03:36 -0500

    02.02.14 fixed tests/ and minor vignette changes

--- tests/tests.r
+++ tests/tests.r
@@ -1,23 +1,23 @@
-library(Rtools, quietly=TRUE)
+library(SEXPtools, quietly=TRUE)
 
 
 ### Lists
 a <- list(abc=1:2, defg=-10.10214)
-b <- test_list(TRUE)
+b <- SEXPtools:::test_list(TRUE)
 all.equal(a, b)
 
 names(a) <- NULL
-b <- test_list(FALSE)
+b <- SEXPtools:::test_list(FALSE)
 all.equal(a, b)
 
 
 ### Dataframes
 a <- data.frame(X1=1:2, X2=c(-10.10214, 1.23456))
-b <- test_df()
+b <- SEXPtools:::test_df()
 all.equal(a, b)
 
 
 ### Printing
-test_print()
+SEXPtools:::test_print()
 
 
