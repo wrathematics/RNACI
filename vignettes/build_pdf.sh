008660058dc0d90b7d229b2983ef4a1341b4f62b
wrathematics <wrathematics@gmail.com>
2014-06-18 18:30:23 -0400

    more vignette

--- vignettes/build_pdf.sh
+++ vignettes/build_pdf.sh
@@ -1,5 +1,9 @@
 #!/bin/sh
 
+PKGVER=`grep "Version:" ../DESCRIPTION | sed -e "s/Version: //"`
+sed -i -e "s/myversion}{.*}/myversion}{${PKGVER}}/" cover.tex
+
+
 fil=`ls | grep "[.]Rnw"`
 
 rm *.aux *.bbl *.blg *.log *.out *.toc
