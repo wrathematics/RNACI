0062715685591bea9a7eb09b10d0bc66df4ed77f
wrathematics <wrathematics@gmail.com>
2014-06-20 17:35:52 -0400

    PACKAGE statements on .Call()'s

--- R/tests.r
+++ R/tests.r
@@ -1,6 +1,6 @@
-test_list <- function(names=TRUE) .Call("test_list", as.integer(names))
+test_list <- function(names=TRUE) .Call("test_list", as.integer(names), PACKAGE="SEXPtools")
 
-test_print <- function() .Call("test_print")
+test_print <- function() .Call("test_print", PACKAGE="SEXPtools")
 
-test_df <- function() .Call("test_df")
+test_df <- function() .Call("test_df", PACKAGE="SEXPtools")
 
