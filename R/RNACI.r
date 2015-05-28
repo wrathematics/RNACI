288d00889006eabb23cd18997556931467992f7b
wrathematics <wrathematics@gmail.com>
2015-05-28 19:51:25 -0400

    switching to roxygen

---
+++ R/RNACI.r
@@ -0,0 +1,14 @@
+#' RNACI
+#' 
+#' TODO
+#'
+#' This package is licensed under the permissive 2-clause BSD license.
+#'
+#' @useDynLib RNACI
+#'
+#' @name RNACI
+#' @docType package
+#' @title RNACI
+#' @author Drew Schmidt \email{wrathematics@@gmail.com} and Wei-Chen Chen
+#' @keywords package
+NULL
--- R/flags.r
+++ R/flags.r
@@ -1,34 +1,56 @@
+#' cppflags
+#' 
+#' The CPPFLAGS for use when linking agains RNACI.
+#' 
+#' @param cmake
+#' For use with CMake or not.
+#' 
+#' @export
 cppflags <- function(cmake=FALSE)
 {
-    inc.path <- tools::file_path_as_absolute(system.file("include", package="RNACI"))
-    
-    if(.Platform$OS.type != "windows"){
-        if (!cmake)
-            flags <- paste("-I", inc.path, sep="")
-        else
-            flags <- inc.path
-    } else{
-        flags <- paste("-I\"", inc.path, "\"", sep="")
-    }
-
-    cat(flags)
+  inc.path <- tools::file_path_as_absolute(system.file("include", package="RNACI"))
+  
+  if(.Platform$OS.type != "windows")
+  {
+    if (!cmake)
+      flags <- paste("-I", inc.path, sep="")
+    else
+      flags <- inc.path
+  } 
+  else
+    flags <- paste("-I\"", inc.path, "\"", sep="")
+  
+  cat(flags)
 }
 
-ldflags <- function(arch = '')
+
+
+#' ldflags
+#' 
+#' The LDFLAGS for use when linking agains RNACI.
+#' 
+#' @param arch
+#' Optional arch value.
+#' 
+#' @export
+ldflags <- function(arch='')
 {
-    if(.Platform$OS.type != "windows"){
-      file.path <- paste("lib/", arch, sep = "")
-
-      ld.path <- tools::file_path_as_absolute(system.file(file.path, package="RNACI"))
-
-      flags <- paste(ld.path, "/librnaci.a", sep="")
-    } else{
-      file.path <- paste("lib/", .Platform$r_arch, sep = "")
-
-      ld.path <- tools::file_path_as_absolute(system.file(file.path, package="RNACI"))
-
-      flags <- paste("\"", ld.path, "/librnaci.a", "\"", sep="")
-    }
-
-    cat(flags)
+  if(.Platform$OS.type != "windows")
+  {
+    file.path <- paste("lib/", arch, sep = "")
+    
+    ld.path <- tools::file_path_as_absolute(system.file(file.path, package="RNACI"))
+    
+    flags <- paste(ld.path, "/librnaci.a", sep="")
+  }
+  else
+  {
+    file.path <- paste("lib/", .Platform$r_arch, sep = "")
+    
+    ld.path <- tools::file_path_as_absolute(system.file(file.path, package="RNACI"))
+    
+    flags <- paste("\"", ld.path, "/librnaci.a", "\"", sep="")
+  }
+  
+  cat(flags)
 }
