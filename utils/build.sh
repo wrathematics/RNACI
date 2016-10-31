75539610ba0130aef37c4f0b73b884ebd2848f67
Drew Schmidt <wrathematics@gmail.com>
2016-10-31 18:47:03 -0400

    add regular build

---
+++ utils/build.sh
@@ -0,0 +1,43 @@
+#!/bin/sh
+
+export PROJROOT="../"
+export OUTDIR="${PROJROOT}/build/regular/"
+
+COMMENT_FILE(){
+  cat $1 | sed -e 's/^/\/\/ /' >> $OUTFILE
+}
+
+ECHO_FILE(){
+  echo -e $1 >> $OUTFILE
+}
+
+CAT_FILE(){
+  cat $1 | sed -e "s/RNACI_FUNTYPE//g" >> $OUTFILE
+}
+
+if [ -d $OUTDIR ]; then
+  rm -rf $OUTDIR
+fi
+
+mkdir $OUTDIR
+
+OUTFILE_H="${OUTDIR}/RNACI.h"
+cp "${PROJROOT}/src/API.h" ${OUTFILE_H}
+sed -i -e "s/RNACI_FUNTYPE//g" ${OUTFILE_H}
+
+OUTFILE="${OUTDIR}/RNACI.c"
+
+ECHO_FILE "// NOTE: file generated automatically from RNACI source; do not edit by hand\n" OUTFILE
+
+COMMENT_FILE "${PROJROOT}/LICENSE.md"
+ECHO_FILE "\n// Changelog:"
+COMMENT_FILE "${PROJROOT}/ChangeLog"
+ECHO_FILE "\n\n"
+
+ECHO_FILE "#include \"RNACI.h\"\n"
+
+for f in `ls ${PROJROOT}/src/*.c`; do
+  ECHO_FILE "// ${f}"
+  CAT_FILE $f
+  ECHO_FILE "\n\n"
+done
