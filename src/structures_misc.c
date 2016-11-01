ea5ea5c3765319a391da7740e6ede47eec71cc79
Drew Schmidt <wrathematics@gmail.com>
2016-11-01 12:46:55 -0400

    avoid unnecessary function call in set_df_colnames()

--- src/structures_misc.c
+++ src/structures_misc.c
@@ -10,7 +10,7 @@ RNACI_FUNTYPE void set_df_rownames(SEXP R_df, SEXP R_rownames)
 
 RNACI_FUNTYPE void set_df_colnames(SEXP R_df, SEXP R_colnames)
 {
-  set_list_names(R_df, R_colnames);
+  setAttrib(R_df, R_NamesSymbol, R_colnames);
 }
 
 RNACI_FUNTYPE void set_list_as_df(SEXP R_list)
