e1223d5f03e99344a9def17cd2033ac8443c8994
Drew Schmidt <wrathematics@gmail.com>
2016-11-04 19:56:17 -0400

    forgot one

--- Rpkg/R/tests.r
+++
@@ -1,5 +0,0 @@
-test_list <- function(names=TRUE) .Call("test_list", as.integer(names), PACKAGE="RNACI")
-
-test_print <- function() .Call("test_print", PACKAGE="RNACI")
-
-test_df <- function() .Call("test_df", PACKAGE="RNACI")
