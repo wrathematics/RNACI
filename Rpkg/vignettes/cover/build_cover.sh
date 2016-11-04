b619720e42a0b1bd97498075e45fb2ab99c4a490
Drew Schmidt <wrathematics@gmail.com>
2016-11-04 19:40:45 -0400

    clean up R package

--- Rpkg/vignettes/cover/build_cover.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-PKGVER=`grep "Version:" ../../DESCRIPTION | sed -e "s/Version: //"`
-sed -i -e "s/thispackageversion}{.*}/thispackageversion}{${PKGVER}}/" cover.tex
-
-xelatex cover.tex
-xelatex cover.tex
-Rscript -e "tools::compactPDF('cover.pdf', gs_quality='ebook')"
-rm *.aux *.bbl *.blg *.log *.out *.toc *.idx *.lof *.lot *.ind *.ilg *.dvi
