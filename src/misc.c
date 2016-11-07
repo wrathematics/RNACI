75290d8f4404ccd96eaf4a38f07bb399053dc220
Drew Schmidt <wrathematics@gmail.com>
2016-11-07 14:28:27 -0500

    fix misc protect/unprotect mismatch

--- src/misc.c
+++ src/misc.c
@@ -3,7 +3,7 @@ RNACI_FUNTYPE int is_Rnull(SEXP x)
   SEXP basePackage;
   SEXP tmp;
   
-  RNACI_PT( basePackage = eval( lang2( install("getNamespace"), ScalarString(mkChar("base")) ), R_GlobalEnv ) );
+  PROTECT( basePackage = eval( lang2( install("getNamespace"), ScalarString(mkChar("base")) ), R_GlobalEnv ) );
   
   tmp = eval( lang2( install("is.null"), x), basePackage);
   
@@ -16,7 +16,7 @@ RNACI_FUNTYPE int is_Rnan(SEXP x)
   SEXP basePackage;
   SEXP tmp;
 
-  RNACI_PT( basePackage = eval( lang2( install("getNamespace"), ScalarString(mkChar("base")) ), R_GlobalEnv ) );
+  PROTECT( basePackage = eval( lang2( install("getNamespace"), ScalarString(mkChar("base")) ), R_GlobalEnv ) );
 
   tmp = eval( lang2( install("is.nan"), x), basePackage);
 
@@ -29,7 +29,7 @@ RNACI_FUNTYPE int is_Rna(SEXP x)
   SEXP basePackage;
   SEXP tmp;
   
-  RNACI_PT( basePackage = eval( lang2( install("getNamespace"), ScalarString(mkChar("base")) ), R_GlobalEnv ) );
+  PROTECT( basePackage = eval( lang2( install("getNamespace"), ScalarString(mkChar("base")) ), R_GlobalEnv ) );
   
   tmp = eval( lang2( install("is.na"), x), basePackage);
   
@@ -42,7 +42,7 @@ RNACI_FUNTYPE int is_double(SEXP x)
   SEXP basePackage;
   SEXP tmp;
   
-  RNACI_PT( basePackage = eval( lang2( install("getNamespace"), ScalarString(mkChar("base")) ), R_GlobalEnv ) );
+  PROTECT( basePackage = eval( lang2( install("getNamespace"), ScalarString(mkChar("base")) ), R_GlobalEnv ) );
   
   tmp = eval( lang2( install("is.double"), x), basePackage);
   
@@ -55,10 +55,11 @@ RNACI_FUNTYPE int is_integer(SEXP x)
   SEXP basePackage;
   SEXP tmp;
   
-  RNACI_PT( basePackage = eval( lang2( install("getNamespace"), ScalarString(mkChar("base")) ), R_GlobalEnv ) );
+  PROTECT( basePackage = eval( lang2( install("getNamespace"), ScalarString(mkChar("base")) ), R_GlobalEnv ) );
   
   tmp = eval( lang2( install("is.integer"), x), basePackage);
   
   UNPROTECT(1);
   return INT(tmp);
 }
+
